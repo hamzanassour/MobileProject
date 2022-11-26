987140868950dbf638868c54e177980f09c62af1
Hnas <hamza.nassour13@gmail.com>
2022-11-26 03:54:15 +0100

    initial commit

---
+++ app/src/main/java/com/ensa/ensaquiz/WalletFragment.java
@@ -0,0 +1,59 @@
+package com.ensa.ensaquiz;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.ensa.ensaquiz.databinding.FragmentWalletBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class WalletFragment extends Fragment {
+
+    public WalletFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    FragmentWalletBinding binding;
+    FirebaseFirestore database;
+    User user;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        binding = FragmentWalletBinding.inflate(inflater, container, false);
+        database = FirebaseFirestore.getInstance();
+
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                user = documentSnapshot.toObject(User.class);
+                binding.currentCoins.setText(String.valueOf(user.getCoins()));
+
+                //binding.currentCoins.setText(user.getCoins() + "");
+
+            }
+        });
+
+
+
+
+        return binding.getRoot();
+    }
+}
